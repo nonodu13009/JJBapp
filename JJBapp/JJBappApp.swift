bc65874ba943fb7336906996ebcafeee101e0bef
nonodu13009 <jeanminono13@gmail.com>
2025-08-25 09:35:47 +0200

    Initial Commit

---
+++ JJBapp/JJBappApp.swift
@@ -0,0 +1,17 @@
+//
+//  JJBappApp.swift
+//  JJBapp
+//
+//  Created by Jean-Michel Nogaro on 25/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct JJBappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
